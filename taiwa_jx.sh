2f643b2c63a80bc8b1e1b371eb4a6db0c83232c7
yiguihai <18776473475@163.com>
2018-06-27 23:58:19 +0800

    Update taiwa_jx.sh

--- taiwa_jx.sh
+++ taiwa_jx.sh
@@ -1,8 +1,8 @@
  x=0
 
-	ip_list=`nslookup bangumi.bilibili.com 8.8.8.8 | egrep -o '[0-9]{1,3}(\.[0-9]{1,3}){3}'|egrep -v $dns`	for j in $ip_list
-
-	do
+	ip_list=`nslookup bangumi.bilibili.com 8.8.8.8 | egrep -o '[0-9]{1,3}(\.[0-9]{1,3}){3}'|egrep -v $dns` 
+	
+	for j in $ip_list do
 
 		((x++))
 
