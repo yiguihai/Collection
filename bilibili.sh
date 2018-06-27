736814129380ce206769dea55bfdfbd51da61d3b
yiguihai <18776473475@163.com>
2018-06-27 16:49:19 +0800

    Create bilibili.sh
    bilibili解析地址

---
+++ bilibili.sh
@@ -0,0 +1,19 @@
+dns="8.8.8.8"
+
+domain_name="bangumi.bilibili.com"
+
+for i in $domain_name
+
+do
+
+ip_list=`nslookup $i $dns | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|egrep -v $dns`
+
+for j in $ip_list
+
+do
+
+iptables -t nat -A out_forward -p tcp -d $j -j DNAT --to-destination 127.0.0.1:3128
+
+done
+
+done
