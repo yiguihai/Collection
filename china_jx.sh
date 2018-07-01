e1223c652586d9d948bc0c65660b3b05a2207e58
yiguihai <18776473475@163.com>
2018-07-01 21:51:27 +0800

    Update china_jx.sh

--- china_jx.sh
+++ china_jx.sh
@@ -1,30 +1,32 @@
 dns="114.114.114.114"
 domain_list=(
-	#腾讯视频(通用)
-	vv.video.qq.com
-	sec.video.qq.com
-	#优酷视频(手机网页版)
-	api.youku.com
-	statis.api.3g.youku.com
-	static.youku.com
-	#爱奇艺(手机网页版)
-	data.video.iqiyi.com
-	cache.m.iqiyi.com
-	#网易云音乐(安卓客户端)
-	
-	mam.netease.com
-	mr.da.netease.com
-	nstool.netease.com
-	music.httpdns.c.163.com
-	#QQ音乐(安卓客户版)
-	c.y.qq.com
-	u.y.qq.com
-	#酷狗音乐(手机网页版)
-	m.kugou.com
-	#酷我音乐(手机网页版)
-	m.kuwo.cn
-	#ip测试
-	ip.cn
+#哔哩哔哩(通用)
+bangumi.bilibili.com
+#腾讯视频(通用)
+vv.video.qq.com
+sec.video.qq.com
+#优酷视频(手机网页版)
+api.youku.com
+statis.api.3g.youku.com
+static.youku.com
+#爱奇艺(手机网页版)
+data.video.iqiyi.com
+cache.m.iqiyi.com
+#网易云音乐(安卓客户端)
+music.163.com
+mam.netease.com
+mr.da.netease.com
+nstool.netease.com
+music.httpdns.c.163.com
+#QQ音乐(手机网页版)
+c.y.qq.com
+u.y.qq.com
+#酷狗音乐(手机网页版)
+m.kugou.com
+#酷我音乐(手机网页版)
+m.kuwo.cn
+#ip测试
+ip.cn
 )
 
 x=0
