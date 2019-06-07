ccb8eea5fb979139e53bb98924179677f0f2a049
yiguihai <18776473475@163.com>
2019-06-07 11:29:03 +0800

    Add files via upload

--- gfwlisttest.sh
+++ gfwlisttest.sh
@@ -15,15 +15,18 @@ plain='\033[0m'
 
 rm -f $(pwd)/fail.acl
 touch $(pwd)/fail.acl
-x=0
+
 url_test()(
     if $3 ; then
-      m=10
+      t=10
+      m=20
+      r="--retry 2"
     else
-      m=5
+      t=5
+      m=8
     fi
-    code=$(curl -L -A "MAUI WAP Browser" -x socks5://127.0.0.1:1080 -m ${m} -s -o /dev/null -w "%{http_code}" "${1}")
-    if [[ "${code}" -ge 200 && "${code}" -le 308 ]]; then
+    code=$(curl -L ${r} -A "MAUI WAP Browser" -x socks5://127.0.0.1:1080 --connect-timeout ${t} -m ${m} -s -o /dev/null -w "%{http_code}" "${1}")
+    if [[ "${code}" -ge 200 ]]; then
       echo -e "${white}${1}${plain} ${green}响应成功! $code ${plain} ${lightred}${x}${plain} "
     else
       if [ -z "$(echo ${1}|grep -E '^(\s|\[|\#|([0-9]{1,3}\.){3}[0-9]{1,3})')" ]; then
@@ -34,39 +37,42 @@ url_test()(
 )
 check(){ 
   local domain=$(echo $(echo ${1//\(\^\|\\\.\)/}|sed -e 's/\\././g')|sed -e 's/\$//g')
-  #if [ "$(echo ${1}|grep -E '^(\s|\[|\#|([0-9]{1,3}\.){3}[0-9]{1,3})')" ]; then
-    #echo ${1} >> $(pwd)/test.acl
-  #else
-    if [ "${domain}" ]; then
-      ((x++))
-      if $2 ; then
-        (url_test "${domain}" "${1}" ${2})&
-      else
-        url_test "${domain}" "${1}" ${2}
-      fi
+  if [ "${domain}" ]; then
+    ((x++))
+    if $2 ; then
+      (url_test "${domain}" "${1}" ${2})&
+    else
+      url_test "${domain}" "${1}" ${2}
     fi
-  #fi
+  fi
 }
+
+x=0
 while IFS= read -r line; do
   check "${line}" true
-  sleep 1
+  sleep 0.1
 done < $(pwd)/gfwlist.acl
 wait
+
 #第二次测试
+x=0
 while IFS= read -r line; do
   check "${line}" false
 done < $(pwd)/fail.acl
-
 sort -u $(pwd)/fail.acl -o $(pwd)/fail.acl #重新整理
 
+echo -e "${lightred}正在整理写入文件...${plain}.."
+x=0
 while IFS= read -r line; do
+  ((x++))
   while IFS= read -r lines; do
     if [[ "${line}" == "${lines}" ]]; then
-      sed -i "/${lines}/d" $(pwd)/gfwlist.acl
+      sed -i "${x}d" $(pwd)/gfwlist.acl
     fi
   done < $(pwd)/fail.acl
 done < $(pwd)/gfwlist.acl
 sed -i '/^$/d' $(pwd)/gfwlist.acl #删除空行
+
 history -cw
 clear
 end_time=$(date +%s)
@@ -75,5 +81,5 @@ hour_distance=$(expr ${time_distance} / 3600)
 hour_remainder=$(expr ${time_distance} % 3600)  
 min_distance=$(expr ${hour_remainder} / 60)  
 min_remainder=$(expr ${hour_remainder} % 60)
-echo -e "测试结果文件: ${lightred}$(pwd)/test.acl${plain}";
-echo -e "测试完成！[总数: ${lightred}${x}${plain}] 耗时 ${white}${hour_distance}:${min_distance}:${min_remainder}${plain}";
+echo -e "测试文件: ${lightred}$(pwd)/fail.acl${plain}";
+echo -e "测试完成！总数: [${lightred}${x}${plain}] 耗时 ${white}${hour_distance}:${min_distance}:${min_remainder}${plain}";
