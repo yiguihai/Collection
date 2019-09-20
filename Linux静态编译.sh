8830b80616ed116cea737080c1e41a332471a902
yiguihai <18776473475@163.com>
2019-09-20 13:50:16 +0800

    Update Linux静态编译.sh

--- Linux静态编译.sh
+++ Linux静态编译.sh
@@ -1,15 +1,19 @@
 #echo | gcc -v -x c++ -E -
-yum install -y gcc 
-yum install -y gcc-c++
-yum install -y clang
-yum install -y autoconf
-yum install -y automake
-yum install -y libtool
-yum install -y gettext
-yum install -y pkg-config
-yum install -y git
-yum install -y wget
-yum install -y unzip
+
+apt_depends=(
+build-essential unzip gzip wget curl autoconf automake gcc make git
+)
+yum_depends=(
+gcc gcc-c++ clang autoconf automake pkg-config git wget curl unzip
+)
+apt-get -y update
+for depend in ${apt_depends[@]}; do
+  apt-get -y install ${depend}
+done
+
+
+libev_ver="4.27"
+libpcre_ver="8.43"
 
 #重新登录
 logout
@@ -23,7 +27,7 @@ cd
 git clone --recursive https://github.com/ARMmbed/mbedtls.git
 cd mbedtls
 git submodule update --init crypto
-make no_test -j4
+make no_test -j
 make install DESTDIR=/root/tmp
 make clean
 
@@ -34,21 +38,21 @@ git clone https://github.com/jedisct1/libsodium --branch stable
 cd libsodium
 ./autogen.sh
 ./configure --prefix=/root/tmp
-make -j4
+make -j
 make install
 make clean
 
 cd 
 
 #libev
-wget http://dist.schmorp.de/libev/libev-4.25.tar.gz
-tar zxvf libev-4.25.tar.gz
-rm -rf libev-4.25.tar.gz
-cd libev-4.25
+wget http://dist.schmorp.de/libev/libev-${libev_ver}.tar.gz
+tar zxvf libev-${libev_ver}.tar.gz
+rm -rf libev-${libev_ver}.tar.gz
+cd libev-${libev_ver}
 chmod +x autogen.sh
 ./autogen.sh
 ./configure --prefix=/root/tmp
-make -j4
+make -j
 make install
 make clean
 
@@ -59,21 +63,21 @@ git clone https://github.com/c-ares/c-ares
 cd c-ares
 ./buildconf
 ./configure --prefix=/root/tmp
-make -j4
+make -j
 make install
 make clean
 
 cd
 
 #libpcre
-wget https://ftp.pcre.org/pub/pcre/pcre-8.43.zip
-unzip pcre-8.43.zip
-cd pcre-8.43
+wget https://ftp.pcre.org/pub/pcre/pcre-${libpcre_ver}.zip
+unzip pcre-${libpcre_ver}.zip
+cd pcre-${libpcre_ver}
 autoreconf -f -i -v
 #cd cmake
 #cmake -DCMAKE_INSTALL_PREFIX:PATH=/root/tmp ..
 ./configure --prefix=/root/tmp
-make -j4
+make -j
 make install
 make clean
 
@@ -85,7 +89,7 @@ cd
 #rm -rf openssl-1.1.1b.tar.gz
 #cd openssl-1.1.1b
 #./config --prefix=/root/tmp LIBS=-llog
-#make -j4
+#make -j
 #make install_sw
 
 #cd
@@ -96,7 +100,7 @@ cd
 #rm -rf zlib-1.2.11.tar.gz
 #cd zlib-1.2.11
 #./configure --prefix=/root/tmp
-#make -j4
+#make -j
 #make install
 #make clean
 #cd
@@ -106,13 +110,13 @@ git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
 git submodule update --init --recursive
 ./autogen.sh
-./configure --disable-documentation --disable-libtool-lock --with-ev=/root/tmp --with-sodium=/root/tmp --with-cares=/root/tmp --with-pcre=/root/tmp --with-mbedtls=/root/tmp --prefix=/root/ss
+./configure --disable-documentation --with-ev=/root/tmp --with-sodium=/root/tmp --with-cares=/root/tmp --with-pcre=/root/tmp --with-mbedtls=/root/tmp --prefix=/root/ss
 
 #查找替换
 find /root/shadowsocks-libev/ -name "Makefile" -type f -exec sed -i 's/-lev  -lcares -lsodium -lmbedcrypto -lpcre/-l:libev.a  -l:libcares.a -l:libsodium.a -l:libmbedcrypto.a -l:libpcre.a/g' {} +
 find /root/shadowsocks-libev/ -name "Makefile" -type f -exec sed -i 's/-lev -lsodium/-l:libev.a -l:libsodium.a/g' {} +
 find /root/shadowsocks-libev/ -name "Makefile" -type f -exec sed -i 's/-lcares/-l:libcares.a/g' {} +
-make -j4
+make -j
 find /root/shadowsocks-libev/src ! -name 'ss-nat' -a -name 'ss-*' -type f | xargs strip
 #find /root/shadowsocks-libev/src ! -name 'ss-nat' -a -name 'ss-*' -type f | xargs upx --best -v
 make install
