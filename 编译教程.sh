b7c7d737ba0db4e2179307f9b61308502cfb4722
yiguihai <18776473475@163.com>
2019-09-20 14:09:11 +0800

    Update 编译教程.sh

--- 编译教程.sh
+++ 编译教程.sh
@@ -1,56 +1,61 @@
+#echo | gcc -v -x c++ -E -
+
+
+libsodium_ver="1.0.18"
+libev_ver="4.27"
+libpcre_ver="8.43"
+
 pkg install -y openssh
 cd .ssh
 termux-setup-storage
 cp /sdcard/id_rsa.pub .
 cat id_rsa.pub >> authorized_keys
 
-pkg install -y clang
-pkg install -y autoconf
-pkg install -y automake
-pkg install -y libtool
-pkg install -y gettext
-pkg install -y pkg-config
-pkg install -y git
-pkg install -y wget
+pkg update
+
+for i in clang autoconf automake libtool gettext pkg-config git wget; do
+  pkg install -y $i
+done
 
 #重新登录
 logout
 
 mkdir /data/data/com.termux/files/home/tmp
+mkdir /data/data/com.termux/files/home/ss
 
 cd 
 
 #mbedtls
 git clone https://github.com/ARMmbed/mbedtls
 cd mbedtls
-make no_test -j4
+make no_test -j
 make install DESTDIR=/data/data/com.termux/files/home/tmp
 make clean
 
 cd 
 
 #libsodium
-wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
-tar zxvf libsodium-1.0.16.tar.gz
-rm -rf libsodium-1.0.16.tar.gz
-cd libsodium-1.0.16
+wget https://github.com/jedisct1/libsodium/releases/download/${libsodium_ver}-RELEASE/libsodium-${libsodium_ver}.tar.gz
+tar zxvf libsodium-${libsodium_ver}.tar.gz
+rm -rf libsodium-${libsodium_ver}.tar.gz
+cd libsodium-${libsodium_ver}
 ./autogen.sh
 ./configure --prefix=/data/data/com.termux/files/home/tmp
-make -j4
+make -j
 make install
 make clean
 
 cd 
 
 #libev
-wget http://dist.schmorp.de/libev/libev-4.25.tar.gz
-tar zxvf libev-4.25.tar.gz
-rm -rf libev-4.25.tar.gz
-cd libev-4.25
+wget http://dist.schmorp.de/libev/libev-${libev_ver}.tar.gz
+tar zxvf libev-${libev_ver}.tar.gz
+rm -rf libev-${libev_ver}.tar.gz
+cd libev-${libev_ver}
 chmod +x autogen.sh
 ./autogen.sh
 ./configure --prefix=/data/data/com.termux/files/home/tmp
-make -j4
+make -j
 make install
 make clean
 
@@ -61,35 +66,35 @@ git clone https://github.com/c-ares/c-ares
 cd c-ares
 ./buildconf
 ./configure --prefix=/data/data/com.termux/files/home/tmp
-make -j4
+make -j
 make install
 make clean
 
 cd
 
 #libpcre
-wget https://ftp.pcre.org/pub/pcre/pcre-8.42.zip
-unzip pcre-8.42.zip
-cd pcre-8.42
+wget https://ftp.pcre.org/pub/pcre/pcre-${libpcre_ver}.zip
+unzip pcre-${libpcre_ver}.zip
+cd pcre-${libpcre_ver}
 autoreconf -f -i -v
 #cd cmake
 #cmake -DCMAKE_INSTALL_PREFIX:PATH=/data/data/com.termux/files/home/tmp ..
 ./configure --prefix=/data/data/com.termux/files/home/tmp
-make -j4
+make -j
 make install
 make clean
 
 cd
 
 #openssl
-wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
-tar zxvf openssl-1.1.1.tar.gz
-rm -rf openssl-1.1.1.tar.gz
-cd openssl-1.1.1
+#wget https://www.openssl.org/source/openssl-1.1.1.tar.gz
+#tar zxvf openssl-1.1.1.tar.gz
+#rm -rf openssl-1.1.1.tar.gz
+#cd openssl-1.1.1
 #./config --prefix=/data/data/com.termux/files/home/tmp LIBS=-llog
-./config --prefix=/data/data/com.termux/files/home/tmp -llog
-make -j4
-make install_sw
+#./config --prefix=/data/data/com.termux/files/home/tmp -llog
+#make -j
+#make install_sw
 
 cd
 
@@ -98,12 +103,15 @@ git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
 git submodule update --init --recursive
 ./autogen.sh
-./configure --disable-documentation --disable-assert --with-pcre=/data/data/com.termux/files/home/tmp --with-mbedtls=/data/data/com.termux/files/home/tmp --with-sodium=/data/data/com.termux/files/home/tmp --with-cares=/data/data/com.termux/files/home/tmp --with-ev=/data/data/com.termux/files/home/tmp LIBS="-llog" --program-prefix=/system
+./configure --disable-documentation --with-pcre=/data/data/com.termux/files/home/tmp --with-mbedtls=/data/data/com.termux/files/home/tmp --with-sodium=/data/data/com.termux/files/home/tmp --with-cares=/data/data/com.termux/files/home/tmp --with-ev=/data/data/com.termux/files/home/tmp LIBS="-llog" --prefix=/data/data/com.termux/files/home/ss
 
 #查找替换
 find /data/data/com.termux/files/home/shadowsocks-libev/ -name "Makefile" -type f -exec sed -i 's/-lev  -lcares -lsodium -lmbedcrypto -lpcre/-l:libev.a  -l:libcares.a -l:libsodium.a -l:libmbedcrypto.a -l:libpcre.a/g' {} +
 find /data/data/com.termux/files/home/shadowsocks-libev/ -name "Makefile" -type f -exec sed -i 's/-lev -lsodium/-l:libev.a -l:libsodium.a/g' {} +
 find /data/data/com.termux/files/home/shadowsocks-libev/ -name "Makefile" -type f -exec sed -i 's/-lcares/-l:libcares.a/g' {} +
+
+#修改源码
+
 make -j4
 find /data/data/com.termux/files/home/shadowsocks-libev/src ! -name 'ss-nat' -a -name 'ss-*' -type f | xargs aarch64-linux-android-strip
 #find /data/data/com.termux/files/home/shadowsocks-libev/src ! -name 'ss-nat' -a -name 'ss-*' -type f | xargs arm-linux-androideabi-strip
