4ae50945352196fa0670af73ffadf9f4332e18b2
yiguihai <18776473475@163.com>
2021-06-22 18:33:23 +0800

    Update 触发.sh

--- 触发.sh
+++ 触发.sh
@@ -3,6 +3,6 @@ curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/yiguihai/shadowsocks_install/actions/workflows/build.yml/dispatches \
   -u "yiguihai:Personal access tokens" \
-  -d '{"ref":"dev"}'
+  -d '{"ref": "dev", "inputs": {"ssh": "no","reset": "no"}}'
   
 curl -u "yiguihai:Personal access tokens" https://api.github.com
